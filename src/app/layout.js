4f5bbe68c223ff36daf4be967245aa4bd5c99885
Mr.be0001 <bittuthakur22221@gmail.com>
2025-10-03 05:27:31 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
   url: "https://hayasaka.vercel.app",
   type: "website",
   icons: [{ rel: "icon", url: Favicon.src }],
-  site_name: "Hayasaka",
+  site_name: "bittuxd",
   manifest: "/manifest.json",
 };
 
